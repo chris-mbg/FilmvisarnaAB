ca063445f39983c698dffac1309c4c0e92b68616
Johnny Lay <johnny.lay.contact@gmail.com>
2021-05-25 20:32:38 +0200

    Import useEffect, useState from React + built register (user) function for UserContext

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,9 +1,42 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const UserContext = createContext();
 
 const UserContextProvider = ({ children }) => {
-  return <UserContext.Provider value={{}}>{children}</UserContext.Provider>;
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  // Registration for new user.
+  const register = async (userInformation) => {
+    try {
+      const response = await fetch(`/api/v1/users/register`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          firstName: userInformation.firstName,
+          lastName: userInformation.lastName,
+          phone: userInformation.phone,
+          email: userInformation.email,
+          password: userInformation.password,
+        }),
+      });
+
+      // If registration was not successful, then throw new error.
+      if (response.status !== 200) {
+        throw new Error();
+      } else {
+        return true;
+      }
+    } catch (err) {
+      return false;
+    }
+  };
+  return (
+    <UserContext.Provider
+      value={{ loggedInUser, setLoggedInUser, register }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 export default UserContextProvider;
