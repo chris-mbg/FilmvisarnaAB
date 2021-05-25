4271904ffa85cc85ef7174d113c113fc1395b470
Johnny Lay <johnny.lay.contact@gmail.com>
2021-05-25 09:57:16 +0200

    Setup for user's registration route and controller

---
+++ api/controllers/userController.js
@@ -0,0 +1,9 @@
+// Import Model
+const User = require("../models/User");
+
+// Import utils
+const { checkEmail, checkPassword } = require("../utilities/utilities");
+
+// POST - register a new user
+
+module.exports = {};
---
+++ api/routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+// Import controller
+const userController = require("../controllers/userController");
+
+/* Register a new user */
+
+module.exports = router;
