4c9454586c5115a2bb54fa60e230efd0bf86b782
khaledh <hassan.khaled@hotmail.se>
2021-05-25 23:21:13 +0200

    Navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,28 @@
-import React, {useState,} from "react";
+import React  from "react";
 import styles from "../css/Navbar.module.css";
+import { Navbar, Nav } from "react-bootstrap";
 
 
-function Navbar() {
-    const [open, setOpen] = useState(false);
+
+function Navbar1() {
+    // const [open, setOpen] = useState(false);
     return (
-        <div>
-            <nav>
-                <div className={styles.logo}>Filmvisarna</div>
-                <ul 
-                className={styles.nav_links} 
-                style={{ transform: open ? "translateX(0px)" : ""}}
-                >
-                    <li>
-                        <a>START</a>
-                    </li>
-                    <li>
-                        <a>OM OSS</a>
-                    </li>
-                    <li>
-                        <a>REGISTRERA</a>
-                    </li>
-                    <li>
-                        <a>LOGGA IN</a>
-                    </li>
-                </ul>
-                <i onClick={() => setOpen(!open)} class="fas fa-bars burger"></i>
-            </nav>
-
-        </div>
+        <Navbar className={styles.nav} collapseOnSelect expand="lg" bg="pink" variant="dark">
+            
+            <Navbar.Brand className={styles.font} href="#home">Filmvisarna</Navbar.Brand>
+           
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
+                
+                <Nav >
+                    <Nav.Link href="#start">START</Nav.Link>
+                    <Nav.Link href="#omoss">OM OSS</Nav.Link>
+                    <Nav.Link href="#registrera">REGISTRERA</Nav.Link>
+                    <Nav.Link href="#loggain">LOGGA IN</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     )
 }
 
-export default Navbar
+export default Navbar1
