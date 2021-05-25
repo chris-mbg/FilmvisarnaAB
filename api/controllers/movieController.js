30c6eb27b6c7fc4b67a2f2e93c111478ae2ac90d
Christina <chrasse@hotmail.com>
2021-05-25 13:42:26 +0200

    Rote and controller to get all movies from DB

---
+++ api/controllers/movieController.js
@@ -0,0 +1,10 @@
+const Movie = require("../models/Movie");
+
+const getAllMovies = async (req, res) => {
+  let movies = await Movie.find().exec();
+  res.json(movies);
+};
+
+module.exports = {
+  getAllMovies,
+};
