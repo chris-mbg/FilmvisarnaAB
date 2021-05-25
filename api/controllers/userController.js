805a651425c8e87903b63878a4bcbe6ed9ac499d
Johnny Lay <johnny.lay.contact@gmail.com>
2021-05-25 19:56:38 +0200

    Small addition - "logs" user in after successful registration (BackEnd)

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -33,6 +33,12 @@ const register = async (req, res) => {
         if (err) {
           return res.sendStatus(400);
         } else {
+          // "Logs" user in after successful registration.
+          req.session.user = result;
+
+          // TESTING: delete this console log after testing.
+          console.log("req.session.user:", req.session.user);
+
           res.status(201).json({
             status: "success",
             message: "Successfully created a new user.",
