1116cd6774859bb2b37a1073a1c55215eefb1f4a
Johnny Lay <johnny.lay.contact@gmail.com>
2021-05-25 20:10:18 +0200

    Created boilerplate for UserContext + imported UserContext for App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import RegistrationPage from "./pages/RegistrationPage";
-import "./css/App.css"
+import UserContextProvider from "./contexts/UserContext";
+import "./css/App.css";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-        <Route exact path="/" component={HomePage}/>
-          <Route exact path="/registration" component={RegistrationPage} />
-        </Switch>
-      </BrowserRouter>
+      <UserContextProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/registration" component={RegistrationPage} />
+          </Switch>
+        </BrowserRouter>
+      </UserContextProvider>
     </div>
   );
 }
