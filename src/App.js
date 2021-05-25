ddc72b64c4655698c37f06aefc4d019e9af45566
susanneeneroth <susanne@centimedia.se>
2021-05-25 11:33:40 +0200

    css folder and css module added for loginmodal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import LoginModal from './components/LoginModal';
+
 function App() {
-  return <div className='App'>Testar</div>;
+  return (
+    <div className='App'>
+      testar
+      <LoginModal />
+    </div>
+  );
 }
 
 export default App;
--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -1,5 +1,19 @@
+import style from '../css/LoginModal.module.css';
+
 const LoginModal = () => {
-  return <h1></h1>;
+  return (
+    <div className={`form-group`}>
+      <h2>Logga in</h2>
+      <form>
+        <label htmlFor='exampleInputEmail'>E-mail</label>
+        <input type='email' className={`form-control`} required />
+        <label htmlFor=''>Lösenord</label>
+        <input type='password' className={`form-control`} required />
+        <button className={`btn ${style.button}`}>Logga in</button>
+      </form>
+      <p>Inte medlen än? Registrera dig här</p>
+    </div>
+  );
 };
 
 export default LoginModal;
