857fe79ecc3ecec1f71a2b9416e3e5db97d00c94
Christina <chrasse@hotmail.com>
2021-05-26 15:28:25 +0200

    Route and (empty) controller func

---
+++ api/controllers/reservationController.js
@@ -0,0 +1,9 @@
+const Reservation = require("../models/Reservation");
+
+const getReservationsForUser = async (req, res) => {
+  
+}
+
+module.exports = {
+  getReservationsForUser
+};
---
+++ api/routes/reservationRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const reservationController = require("../controllers/reservationController");
+
+router.get("/user", movieController.getReservationsForUser);
+
+
+module.exports = router;
