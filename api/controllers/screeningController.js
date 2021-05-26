2086e4f53e90e50e1098bda5486cb3efe4d24ab0
Christina <chrasse@hotmail.com>
2021-05-26 14:05:05 +0200

    Route and controllerfunc added

--- api/controllers/screeningController.js
+++ api/controllers/screeningController.js
@@ -1,5 +1,17 @@
 const Screening = require("../models/Screening");
 
+const getScreeningsByMovieId = async (req, res) => {
+  const { movieId } = req.params;
+
+  try {
+    // .find() with movieId as filter parameter
+    let screenings = await Screening.find({ movieId: movieId }).exec();
+    console.log(screenings.length);
+    return res.json(screenings);
+  } catch (error) {
+    res.status(400).json({ status: "error", message: error.message });
+  }
+};
 
 module.exports = {
   getScreeningsByMovieId,
--- api/routes/screeningRoutes.js
+++ api/routes/screeningRoutes.js
@@ -4,4 +4,6 @@ const router = express.Router();
 // Import controller
 const screeningController = require("../controllers/screeningController");
 
+router.get("/:movieId", screeningController.getScreeningsByMovieId);
+
 module.exports = router;
