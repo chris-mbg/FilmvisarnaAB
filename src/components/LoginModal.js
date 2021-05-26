cbce512754ed02bd020961e7f23a4406e0f9d076
susanneeneroth <susanne@centimedia.se>
2021-05-26 14:22:55 +0200

    Login modal ready for backend and usercontext

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -1,21 +1,28 @@
-// import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
 
 import style from '../css/LoginModal.module.css';
 
-const LoginModal = (props) => {
-  // const { users, setLoggedInUser } = useContext();
+const LoginModal = () => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
 
   return (
     <div className={`form-group ${style.modalContainer}`}>
       <h2 className={style.loginHeading}>Logga in</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
+          onChange={(e) => setUsername(e.target.value)}
           type='email'
           className={`form-control ${style.input}`}
           required
           placeholder='E-post'
         />
         <input
+          onChange={(e) => setPassword(e.target.value)}
           type='password'
           className={`form-control`}
           required
@@ -25,15 +32,7 @@ const LoginModal = (props) => {
           Logga in
         </button>
       </form>
-      {/* <NavLink
-        exact
-        to='/register'
-        className={`${style.regText}`}
-        onClick={() => props.setShowLoginModal(false)}
-      >
-        Inte medlen än? Registrera dig här
-      </NavLink> */}
-      <p className={style.regText}>Inte medlem än? Registrera dig här</p>
+      <p className={style.regText}>Inte medlem än? Registrera dig här.</p>
     </div>
   );
 };
