a3693d837cde0ef26fbae5c174fea5b635aeae08
Johnny Lay <johnny.lay.contact@gmail.com>
2021-05-26 19:36:56 +0200

    Created handlers + imported useStates

--- src/components/profile/ProfileForm.js
+++ src/components/profile/ProfileForm.js
@@ -1,7 +1,39 @@
 import styles from "../../css/ProfileForm.module.css";
+import { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 
 const ProfileForm = () => {
+  const user = { firstName: "Lorem", lastName: "Ipsum", phoneNumber: "0700000000", email: "admin@mail.com", password: "123456789" }; // Dummy data
+
+  const [firstName, setFirstName] = useState(); // Placeholder data
+  const [lastName, setLastName] = useState(""); // Placeholder data
+  const [phone, setPhone] = useState(""); // Placeholder data
+  const [email, setEmail] = useState(""); // Placeholder data
+  const [password, setPassword] = useState(""); // Placeholder data
+
+  //   const [firstNameEdit, setFirstNameEdit] = useState(false);
+
+  // Handlers
+  const handleFirstName = () => {};
+
+  const handleLastName = () => {};
+
+  const handlePhone = () => {};
+
+  const handleEmail = () => {};
+
+  const handlePassword = () => {};
+
+  const handleFirstNameEdit = () => {};
+
+  const handleLastNameEdit = () => {};
+
+  const handlePhoneEdit = () => {};
+
+  const handleEmailEdit = () => {};
+
+  const handlePasswordEdit = () => {};
+
   return (
     <form className={styles.form}>
       <Row noGutters>
