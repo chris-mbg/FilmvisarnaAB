5c5fc769582d4bb89ba380fb8d3340434054649b
Christina <chrasse@hotmail.com>
2021-05-26 11:56:52 +0200

    Imported Link fr react-router-dom and styling
    Co-authored-by: Khaled <hassan.khaled@hotmail.se>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import "./css/App.css"
-
 import React from "react";
 import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-    <Navbar />
+        <Navbar />
         <Route exact path="/" component={HomePage}/>
       </BrowserRouter>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,49 @@
-import React  from "react";
+import React from "react";
 import styles from "../css/Navbar.module.css";
-import { Navbar, Nav } from "react-bootstrap";
-
-
+import { Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function Navbar1() {
-    // const [open, setOpen] = useState(false);
-    return (
-        <Navbar className={styles.nav} collapseOnSelect expand="lg" bg="pink" variant="dark">
-            
-            <Navbar.Brand className={styles.font} href="#home">Filmvisarna</Navbar.Brand>
-           
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
-                
-                <Nav >
-                    <Nav.Link href="#start">START</Nav.Link>
-                    <Nav.Link href="#omoss">OM OSS</Nav.Link>
-                    <Nav.Link href="#registrera">REGISTRERA</Nav.Link>
-                    <Nav.Link href="#loggain">LOGGA IN</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    )
+  // Import variable from UserContext here...
+  const userLoggedIn = false;
+
+  return (
+    <Navbar className={styles.nav} collapseOnSelect expand="md" variant="dark">
+      <Navbar.Brand as={Link} to="/">
+        <span className={styles.font}>Filmvisarna</span>
+      </Navbar.Brand>
+
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse
+        className="justify-content-end"
+        id="responsive-navbar-nav"
+      >
+        <Nav className={styles.nav_links}>
+          <Nav.Link as={Link} to="/" className={styles.link}>
+            START
+          </Nav.Link>
+          <Nav.Link as={Link} to="/about" className={styles.link}>
+            OM OSS
+          </Nav.Link>
+          {userLoggedIn ? (
+            <Nav.Link as={Link} to="/profile" className={styles.link}>
+              MIN PROFIL
+            </Nav.Link>
+          ) : (
+            <Nav.Link as={Link} to="/register" className={styles.link}>
+              REGISTRERA
+            </Nav.Link>
+          )}
+          {userLoggedIn ? (
+            <Nav.Link className={styles.link}>LOGGA UT</Nav.Link>
+          ) : (
+            <Nav.Link className={styles.link}>LOGGA IN</Nav.Link>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
-export default Navbar1
+export default Navbar1;
