1489c6ba021c16ad17a64fcbd4ca958e64e52949
Christina <chrasse@hotmail.com>
2021-05-27 10:06:13 +0200

    Added condition to DB query to only get future screenings

--- api/controllers/screeningController.js
+++ api/controllers/screeningController.js
@@ -4,8 +4,13 @@ const getScreeningsByMovieId = async (req, res) => {
   const { movieId } = req.params;
 
   try {
-    // .find() with movieId as filter parameter
-    let screenings = await Screening.find({ movieId: movieId }).exec();
+    // .find() with movieId as filter parameter and all screenings regardless of date
+    //let screenings = await Screening.find({ movieId: movieId }).exec();
+
+    // Screenings from when the request is made and onwards
+    const now = new Date();
+    console.log(now)
+    let screenings = await Screening.find({ movieId: movieId }).where('startTime').gte(now).sort("startTime").exec();
     return res.json(screenings);
   } catch (error) {
     res.status(400).json({ status: "error", message: error.message });
