496bec1af94ebe2d1446f263bf6379d5ea0992c0
Johnny Lay <johnny.lay.contact@gmail.com>
2021-05-27 13:24:14 +0200

    Css styling for ReservationListing

--- src/components/profile/ReservationListing.js
+++ src/components/profile/ReservationListing.js
@@ -1,7 +1,11 @@
 import styles from "../../css/ReservationListing.module.css";
 
 const ReservationListing = () => {
-  return <div className={styles.listing_wrapper}></div>;
+  return (
+    <div className={styles.listing_wrapper}>
+      <h2 className={styles.title}>Bokade biljetter</h2>
+    </div>
+  );
 };
 
 export default ReservationListing;
