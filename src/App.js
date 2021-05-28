6c8c6a7c79c37c341b6a66c27207ded320c33454
susanneeneroth <susanne@centimedia.se>
2021-05-28 11:10:28 +0200

    Ändrat tillbaka pga filer saknas vid dev pull

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import HomePage from './pages/HomePage';
+// import MoviePage from './pages/MoviePage';
+// import OrderPage from './pages/OrderPage';
+// import RegistrationPage from './pages/RegistrationPage';
+// import ProfilePage from './pages/ProfilePage';
+// import UserContextProvider from './contexts/UserContext';
+// import MovieContextProvider from './contexts/MovieContext';
+// import './css/App.css';
 import LoginModal from './components/LoginModal';
 
 function App() {
   return (
     <div className='App'>
-      testar
       <LoginModal />
     </div>
   );
