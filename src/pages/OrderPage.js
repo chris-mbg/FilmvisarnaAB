6d9bddd93b216f106e8364ac2a060e5f4f3a2474
Svitlana <devop.edu@gmail.com>
2021-05-29 19:08:40 +0200

    implemented logic for changing the rendering of the auditorium matrix

--- src/pages/OrderPage.js
+++ src/pages/OrderPage.js
@@ -1,13 +1,62 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { Row, Col } from "react-bootstrap";
 import { MovieContext } from "../contexts/MovieContext";
 
-
+// For this page is necessarily (otherwise page not be loaded) to click the "BOOK" button, Because screening id  is needed for to find  the desired movie !!!!!
 export default function OrderPage() {
-  const { getAllScreeningsForMovie } = useContext(MovieContext);
+  const {
+    getMovieById,
+    orderScreenings,
+  } = useContext(MovieContext);
+
+// to render information about the current movie
+  const [currentMovie, setCurremtMovie] = useState(null);
+// for rendering a matrix of seats in a particular auditorium (dynamically changes, depending on input select)
+  const [selectedScreening, setSelectedScreening] = useState(null)
+
+  useEffect(async () => {
+    //  geted from MovieCard when was click on the "BOKA" button
+    if (orderScreenings) {
+      let movie = await getMovieById(orderScreenings[0].movieId);
+      setCurremtMovie(movie);
+    // todo delete console log
+      console.log(movie);
+      console.log(orderScreenings);
+    }
+  }, []);
+
+  const getSelectedScreening = (e) => {
+    setSelectedScreening(orderScreenings.filter(screen => screen.startTime === e.target.value))
+  }
+
+  const renderScreenings = () => (
+    <Row>
+    <Col>{currentMovie.title}</Col>
+    <Col>
+      <select  onChange={(e) => getSelectedScreening(e)}>
+        {orderScreenings.map((time) => (
+          <option 
+          key={time.startTime} 
+          value={time.startTime} 
+        >
+            {new Date (time.startTime).toLocaleString('sv-SE')}
+          </option>
+        ))}
+      </select>
+    </Col>
+  </Row>
+
+  )
+  
 
   return (
     <div>
-      <h1>Order Page</h1>
+      <h1>Boka bilijetter</h1>
+      {orderScreenings && currentMovie ? (
+        renderScreenings()
+      ) : (
+        <h2>...loading</h2>
+      )}
     </div>
-  )
+  );
 }
