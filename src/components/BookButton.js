85646cd4d31ca575816a230267762601a9ba31e7
Christina <chrasse@hotmail.com>
2021-05-29 20:20:30 +0200

    Styling and tooltip when no loggedInUser

--- src/components/BookButton.js
+++ src/components/BookButton.js
@@ -1,27 +1,30 @@
 import { useContext } from "react";
 import { useHistory } from "react-router-dom";
 import { ReservationContext } from "../contexts/ReservationContext";
+import { UserContext } from "../contexts/UserContext";
 import CustomButton from "./CustomButton";
+import styles from "../css/BookButton.module.css"
 
 const BookButton = ({ movieId, movieTitle, screeningId = null }) => {
 
-  console.log("movieId", movieId);
-  console.log("screeningId", screeningId);
-
+  const { loggedInUser } = useContext(UserContext);
   const { setMovieOnOrderPage, setScreeningIdOnOrderPage } = useContext(ReservationContext);
   const history = useHistory();
 
   const bookButtonClick = () => {
-    setMovieOnOrderPage({movieId, movieTitle});
-    if(screeningId) {
-      setScreeningIdOnOrderPage(screeningId)
+    if(loggedInUser) {
+      setMovieOnOrderPage({movieId, movieTitle});
+      if(screeningId) {
+        setScreeningIdOnOrderPage(screeningId)
+      }
+      history.push("/order");
     }
-    history.push("/order");
   }
 
   return (
-    <div>
+    <div className={loggedInUser ? `${styles.bookButton}` : `${styles.bookButton} ${styles.tooltip} ${styles.disabled}`}>
       <CustomButton text={"Boka"} clickHandler={bookButtonClick}/>
+      {!loggedInUser && <span className={styles.tooltiptext}>Logga in för att boka biljetter!</span>}
     </div>
   );
 }
