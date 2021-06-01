27d5c7e3acaaded16e54938c4f5eecbe73e2f34c
susanneeneroth <susanne@centimedia.se>
2021-06-01 13:06:37 +0200

    Just a test

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,14 @@
-import MovieWrapper from '../components/MovieWrapper'
-import ImgCarousel from "../components/ImgCarousel"
+import MovieWrapper from '../components/MovieWrapper';
+import ImgCarousel from '../components/ImgCarousel';
+import MovieCard from '../components/MovieCard';
 
 const HomePage = () => {
   return (
-    <div className="homePage">
+    <div className='homePage'>
       <ImgCarousel />
-      <MovieWrapper/>
+      <MovieWrapper />
     </div>
   );
-}
+};
 
 export default HomePage;
