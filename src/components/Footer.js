b5ba49f0d7b226dc030c9a9195ded04be5f12aa2
khaledh <hassan.khaled@hotmail.se>
2021-06-01 13:18:29 +0200

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "../css/Footer.module.css";
+
+const Footer = () => {
+    return (
+        <div className={styles.mainFooter}  >
+                <div className="row">
+                    <div className="col">
+                        <ul className={styles.logo}>
+                            <li>Filmvisarna</li>
+                        </ul>
+
+                    </div>
+                    <div className="col">
+                            <p>Adressgatan 11
+                            123 45 Staden</p>
+                    </div>
+                    <div className="col">
+                        <ul className={styles.socialIcon}>
+                            <li className={`${styles.icon} fab fa-facebook-f`}></li>
+                            <li className={`${styles.icon} fab fa-instagram`}></li>
+                            <li className={`${styles.icon} fab fa-twitter`}></li>
+                        </ul>
+
+                    </div>
+                </div>
+                <div>
+                    <p className={`${styles.copy}`}>
+                    &copy;{new Date().getFullYear()}FILMVISARNA AB   Copyright Vi använder cookies Integritetspolicy
+                    </p>
+                </div>
+        </div>
+
+    )
+}
+
+export default Footer;
