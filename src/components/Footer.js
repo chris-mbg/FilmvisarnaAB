2eeab24721dc1b7a195fd2e543d221ad8ee1a521
Christina <chrasse@hotmail.com>
2021-06-02 12:42:44 +0200

    Footer done. Small styling changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,33 +3,28 @@ import styles from "../css/Footer.module.css";
 
 const Footer = () => {
     return (
-        <div className={styles.mainFooter}  >
-                <div className="row">
+        <footer className={`${styles.mainFooter}`}  >
+                <div className="row d-flex align-items-center mb-2 mb-md-3">
                     <div className="col">
-                        <ul className={styles.logo}>
-                            <li>Filmvisarna</li>
-                        </ul>
-
+                        <p className={styles.logo}>Filmvisarna</p>
                     </div>
-                    <div className="col">
-                            <p className={styles.adresstext}>Adressgatan 11
-                            123 45 Staden</p>
+                    <div className="col align-items-center d-none d-sm-block">
+                            <address className={styles.adresstext}>
+                                Adressgatan 11, 123 45 Staden
+                            </address>
                     </div>
-                    <div className="col">
-                        <ul className={styles.socialIcon}>
-                            <li className={`${styles.icon} fab fa-facebook-f`}></li>
-                            <li className={`${styles.icon} fab fa-instagram`}></li>
-                            <li className={`${styles.icon} fab fa-twitter`}></li>
-                        </ul>
-
+                    <div className="col offset-sm-1 col-sm-3 offset-xl-2 col-xl-2 d-flex justify-content-around align-items-center">
+                        <i className={`${styles.icon} fab fa-facebook-f`}></i>
+                        <i className={`${styles.icon} fab fa-instagram`}></i>
+                        <i className={`${styles.icon} fab fa-twitter`}></i>
                     </div>
                 </div>
                 <div>
                     <p className={`${styles.copy}`}>
-                    &copy;{new Date().getFullYear()}FILMVISARNA AB | Copyright | Vi använder cookies | Integritetspolicy
+                    &copy;{new Date().getFullYear()}FILMVISARNA AB <br className="d-sm-none"/> <span className="d-none d-sm-inline" >| </span> Copyright | Vi använder cookies | Integritetspolicy
                     </p>
                 </div>
-        </div>
+        </footer>
 
     )
 }
