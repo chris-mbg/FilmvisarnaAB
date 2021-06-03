99b61f8fb148a25283d6795df8309084564cb0e7
Johnny Lay <johnny.lay.contact@gmail.com>
2021-06-03 10:43:17 +0200

    Css styling for confirm modal

--- src/components/order/ConfirmModal.js
+++ src/components/order/ConfirmModal.js
@@ -1,7 +1,16 @@
 import styles from "../../css/ConfirmModal.module.css";
 
 const ConfirmModal = () => {
-  return <div></div>;
+  return (
+    <div className={styles.modal_container}>
+      <h2 className={styles.header}>Biljetter bokade!</h2>
+
+      <div className={styles.button_confirm_wrapper}>
+        <button className={`${styles.button_confirm} btn`}>OK</button>
+      </div>
+      {/* /.button_wrapper */}
+    </div>
+  );
 };
 
 export default ConfirmModal;
