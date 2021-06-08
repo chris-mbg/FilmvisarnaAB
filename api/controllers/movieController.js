bf4941a879e51e046e4d2feed01ac3f7f737b686
Svitlana <devop.edu@gmail.com>
2021-06-08 17:43:18 +0200

    created search function for movies filtering

--- api/controllers/movieController.js
+++ api/controllers/movieController.js
@@ -10,17 +10,58 @@ const getAllMovies = async (req, res) => {
   }
 };
 
-
 const getMovieById = async (req, res) => {
-  Movie.findById(req.params.movieId).exec(async(err, movie) => {
-    if(err) {
-      throw new Error ('error message: ', err)
+  Movie.findById(req.params.movieId).exec(async (err, movie) => {
+    if (err) {
+      throw new Error("error message: ", err);
     }
-    return res.json(movie)
-  })
-}
+    return res.json(movie);
+  });
+};
+
+const getFilteredMovies = async (req, res) => {
+  console.log("QUERY", req.query);
+
+  if (Object.keys(req.query).length === 0) {
+    try {
+      let movies = await Movie.find().exec();
+      return res.json(movies);
+    } catch (error) {
+      res.status(500).json({ status: "error", message: error.message });
+      throw error;
+    }
+  } else {
+    try {
+      const obj = {
+        // ageLimit: "PG-11",
+        director: "Don Hall",
+        language: "Engelska",
+        // length: "123",
+      };
+
+      const queryObj = [];
+
+      for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          let object = {};
+          object[key] = obj[key]
+          queryObj.push(object)
+        }
+      }
+
+      console.log("::queryObj:::", queryObj, typeof queryObj);
+
+      let movies = await Movie.find({$or: queryObj}).exec();
+      return res.json(movies);
+    } catch (error) {
+      res.status(500).json({ status: "error", message: error.message });
+      throw error;
+    }
+  }
+};
 
 module.exports = {
   getAllMovies,
   getMovieById,
+  getFilteredMovies,
 };
--- api/routes/movieRoutes.js
+++ api/routes/movieRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 const movieController = require("../controllers/movieController");
 
-router.get("/", movieController.getAllMovies);
+// router.get("/", movieController.getAllMovies);
 router.get("/:movieId", movieController.getMovieById);
 
+router.get("", movieController.getFilteredMovies);
 module.exports = router;
