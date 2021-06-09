bfb3b5620d1bdaa84a729fbfcb6b119b3391c451
susanneeneroth <susanne@centimedia.se>
2021-06-09 11:45:17 +0200

    Added some styling to NotFoundPage component and changed some styling in LoginPage

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,5 +1,21 @@
+import { NavLink } from 'react-router-dom';
+import style from '../css/NotFoundPage.module.css';
+
 const NotFoundPage = () => {
-  return <h1>404 Page Not Found</h1>;
+  return (
+    <div className={`container ${style.mainContainer}`}>
+      <h1 className={style.notFoundHeading}>
+        404! Sidan kunde tyvärr inte hittas...
+      </h1>
+      <p className={style.paraText}>
+        Det kan bero på ett stavfel eller att sidan inte finns längre. <br />
+        Gå tillbaka till startsidan.
+      </p>
+      <NavLink to='/' className={`${style.linkStyle}`}>
+        <button className={`btn ${style.homeBtn}`}>Start</button>
+      </NavLink>
+    </div>
+  );
 };
 
 export default NotFoundPage;
