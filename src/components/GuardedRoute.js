9a90918f37c7f3f9a895a86dc2def6ebba765477
susanneeneroth <susanne@centimedia.se>
2021-06-11 13:38:30 +0200

    Testing of code

--- src/components/GuardedRoute.js
+++ src/components/GuardedRoute.js
@@ -1,16 +1,28 @@
 import React, { useContext } from 'react';
+import { UserContext } from '../contexts/UserContext';
 import { Component } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import Login from '../components/login/Login';
+import { Modal } from 'react-bootstrap';
+import ModalHeader from 'react-bootstrap/ModalHeader';
+import style from '../css/Login.module.css';
+
+// const {
+//   handleCloseLoginModal,
+//   handleShowLoginModal,
+//   showLogin,
+//   loggedInUser,
+//   logout,
+// } = useContext(UserContext);
 
 const GuardedRoute = ({ component: Component, auth, ...rest }) => (
   <Route
     {...rest}
     render={(props) =>
       auth === true ? (
-        <Component {...props} />
+        <Component {...rest} {...props} />
       ) : (
-        <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+        <Redirect to='/registration' component={Login} />
       )
     }
   />
