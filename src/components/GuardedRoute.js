2e1b3e77f2001258d21edcc2a556e4013be3a86d
susanneeneroth <susanne@centimedia.se>
2021-06-15 13:47:21 +0200

    Changed back to original code without login modal
    Co-author-by: Johnny <johnny.lay.contact@gmail.com>

--- src/components/GuardedRoute.js
+++ src/components/GuardedRoute.js
@@ -1,26 +1,18 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../contexts/UserContext';
-import { Component } from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import Login from '../components/login/Login';
-import { Modal } from 'react-bootstrap';
-import ModalHeader from 'react-bootstrap/ModalHeader';
-import style from '../css/Login.module.css';
 
 const GuardedRoute = ({ component: Component, ...rest }) => {
-  const { setShowLogin, loggedInUser } = useContext(UserContext);
-  console.log(loggedInUser);
-  console.log(rest);
+  const { loggedInUser } = useContext(UserContext);
   return (
     <Route
       {...rest}
       render={(props) =>
-        loggedInUser !== null || undefined ? (
-          <Component {...rest} {...props} />
+        loggedInUser ? (
+          <Component {...props} />
         ) : (
           <>
-            {setShowLogin(true)}
-            <Redirect exact path to='/' component={Login} />
+            <Redirect exact to='/' />
           </>
         )
       }
